d3acc15a7aaaaa56b17806a40e3053cac8f59fc8
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-06 02:11:39 +0330

    Some ES6 migration

--- routes/members.js
+++ routes/members.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const Member = require('../server/model/Member');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
