156db878385711ac4a069d71854925805eecde12
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-06 17:30:02 +0330

    validators added to Schema

--- server/model/Member.js
+++ server/model/Member.js
@@ -1,25 +1,68 @@
 const db = require('./db')
 const moment = require('moment')
+const validator = require('validator')
 
 const schema = db.Schema({
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     name: {
-        farsi: String,
-        latin: String
+        farsi: {
+            type: String,
+            required: true
+        },
+        latin: {
+            type: String,
+            required: true
+        }
     },
     lastname: {
-        farsi: String,
-        latin: String
+        farsi: {
+            type: String,
+            required: true
+        },
+        latin: {
+            type: String,
+            required: true
+        }
     },
     gender: {
         type: String,
         enum: [ 'M', 'F', 'O' ],
+        required: true
     },
-    birthday: String,
-    avatar: String,
-    phone: String,
-    email: String,
+    birthday: {
+        type: String,
+        enum: [ 'M', 'F', 'O' ],
+        required: true
+    },
+    phone: {
+        type: String,
+        enum: [ 'M', 'F', 'O' ],
+        required: true,
+        validate: {
+            isAsync: true,
+            validator: (phoneNumber, cb) => {
+                const phoneRegex = /09\d{9}/;
+                const msg = phoneNumber + ' is not a valid phone number!';
+                cb(phoneRegex.test(phoneNumber), msg);
+            }
+        },
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            isAsync: true,
+            validator: validator.isEmail
+        }
+    },
     membership: {
         start: { type: Date, default: Date.now },
         expire: { type: Date, default: Date.now }
@@ -30,8 +73,8 @@ schema.methods.introduce = function () {
     console.log(`Hey! This is ${this.name}`)
 }
 schema.methods.payFor = function (howManyMonths) {
-    const months = howManyMonths || 1
-    this.membership.expire = moment().add(months, 'months').valueOf()
+    const months = howManyMonths || 1;
+    this.membership.expire = moment().add(months, 'months').valueOf();
 }
 
 schema.methods.getJSON = function () {
@@ -48,24 +91,24 @@ schema.methods.getJSON = function () {
 }
 
 schema.statics.findOneByUsername = function (username, cb) {
-    return this.findOne({ username: new RegExp(username, 'i') }, cb)
+    return this.findOne({ username: new RegExp(username, 'i') }, cb);
 }
 
 schema.virtual('daysToExpire').get(function () {
-    return moment().diff(this.membership.expire, 'days')
+    return moment().diff(this.membership.expire, 'days');
 })
 schema.virtual('isActive').get(function () {
     return moment().diff(this.membership.expire) > 0
 })
 schema.virtual('age').get(function () {
-    const birthday = moment(this.birthday, 'YYYY-MM-DD')
-    return moment().diff(birthday, 'years')
+    const birthday = moment(this.birthday, 'YYYY-MM-DD');
+    return moment().diff(birthday, 'years');
 })
 schema.virtual('farsiFullName').get(function () {
-    return this.name.farsi + ' ' + this.lastname.farsi
+    return this.name.farsi + ' ' + this.lastname.farsi;
 })
 schema.virtual('latinFullName').get(function () {
-    return this.name.latin + ' ' + this.lastname.latin
+    return this.name.latin + ' ' + this.lastname.latin;
 })
 
-module.exports = db.model('Member', schema);
+module.exports = db.model('Member', schema)
