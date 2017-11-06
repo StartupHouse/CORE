69a0fcd48cceb60a4d4af5cb08dabd0fecfe1c68
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-06 19:29:28 +0330

    Replacing global Promise with mpromise

--- server/model/db.js
+++ server/model/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const config = require('../../core.config')
 
+mongoose.Promise = global.Promise
 mongoose.connect(`${config.db.host}${config.db.dbname}`, { useMongoClient: true })
 
 module.exports = mongoose;
