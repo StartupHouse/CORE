065412456f138fd30ad568b00c992cbe5ca8cdfb
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-06 16:59:06 +0330

    Add Members by API added

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Models
 const Member = require('../server/model/Member');
 
 /* GET members list. */
@@ -12,15 +13,35 @@ router.get('/', (req, res) => {
     })
 });
 
-/* GET member by Username. */
-router.get('/:username', (req, res) => {
-    Member.findOneByUsername(req.params.username, (err, member) => {
-        res.json(member.getJSON())
+/* ADD new member */
+router.post('/', (req, res) => {
+
+    // Checking all required fields
+    for (let requiredField of required) if (!req.body[requiredField]) return res.status(400).json({
+        message: `${requiredField} needed to create a new user`
+    });
+
+    // checking username existing
+    Member.findOneByUsername(req.body.username, (err, member) => {
+        // if user existed before
+        if (member) return res.status(400).json({ message: `${req.body.username} username exists` });
+
+        // create new user
+        const newMember = new Member(req.body);
+        newMember.save((err) => {
+
+            // if it couldn't save
+            if (err) return res.status(400).json({ message: err });
+
+            // if it successfully saved!
+            res.status(200).json({ message: `${req.body.username} added successfully` })
+        })
     })
 });
 
-router.post('/', (req, res) => {
-    res.json(req.body)
+/* GET member by Username. */
+router.get('/:username', (req, res) => {
+    Member.findOneByUsername(req.params.username, (err, member) => res.json(member.getJSON()))
 });
 
 module.exports = router;
