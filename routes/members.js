9340ea2fbe7839bd2fd6638ed88cca13a0231b71
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-11-06 17:52:54 +0330

    Schema validate everything now

--- routes/members.js
+++ routes/members.js
@@ -15,27 +15,13 @@ router.get('/', (req, res) => {
 
 /* ADD new member */
 router.post('/', (req, res) => {
+    const newMember = new Member(req.body)
+    newMember.save((err) => {
+        // if it couldn't save
+        if (err) return res.status(400).json(err.errors)
 
-    // Checking all required fields
-    for (let requiredField of required) if (!req.body[requiredField]) return res.status(400).json({
-        message: `${requiredField} needed to create a new user`
-    })
-
-    // checking username existing
-    Member.findOneByUsername(req.body.username, (err, member) => {
-        // if user existed before
-        if (member) return res.status(400).json({ message: `${req.body.username} username exists` })
-
-        // create new user
-        const newMember = new Member(req.body)
-        newMember.save((err) => {
-
-            // if it couldn't save
-            if (err) return res.status(400).json({ message: err })
-
-            // if it successfully saved!
-            res.status(200).json({ message: `${req.body.username} added successfully` })
-        })
+        // if it successfully saved!
+        res.status(200).json({ message: `${req.body.username} added successfully` })
     })
 })
 
